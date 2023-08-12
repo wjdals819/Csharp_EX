e968ebbf902b30c1509703e78f98ec3536f1eb47
wjdals819 <wjdals819@github.com>
2023-08-12 16:36:31 +0900

    매개변수를 제곱하여 반환하는 Square 메소드 작성.

--- Program.cs
+++ Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace Csharp_EX
 {
-    class Program
+    class Ex6_1
     {
+        static double Square(double arg)
+        {
+            double square = arg * arg; //매개변수를 제곱하여 square 변수에 저장
+            return square; //square 변수 반환
+        }
         static void Main(string[] args)
         {
+            Console.Write("수를 입력하세요: ");
+            string input = Console.ReadLine(); //사용자로부터 입력받은 문자를 input에 저장
+
+            double arg = Convert.ToDouble(input); //input의 값을 double형으로 형변환
+
+            Console.WriteLine("결과: {0}", Square(arg));
         }
     }
 }
